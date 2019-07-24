8a460f5f6f7eb371cd5d335f1f750c4636d5d038
Vinicius Rodrigues Rocha <rochaskingdom@gmail.com>
2019-07-24 11:19:21 -0300

    Projeto criado

---
+++ src/main/java/com/viniciusrocha/modeloconceitual/ModeloConceitualApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusrocha.modeloconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloConceitualApplication.class, args);
+	}
+
+}
